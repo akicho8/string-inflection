a26e88beb6f5ede9660dd59e995aa388242fdf74
akicho8 <akicho8@gmail.com>
2014-01-12 00:29:13 +0900

    Fix description

--- string-inflection.el
+++ string-inflection.el
@@ -4,7 +4,7 @@
 
 ;; Author: Akira Ikeda <pinpon.ikeda@gmail.com>
 ;; Keywords: elisp
-;; Version: 1.0.0
+;; Version: 1.0.1
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
