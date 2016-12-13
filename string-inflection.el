cc7a0cf05d7084bfa6f5347c6d6a3f47c62e3f4b
akicho8 <akicho8@gmail.com>
2016-12-13 21:34:01 +0900

    Update README.org

--- string-inflection.el
+++ string-inflection.el
@@ -27,30 +27,48 @@
 ;;
 ;;   1. For Ruby -> string-inflection-ruby-style-cycle  (foo_bar => FOO_BAR => FooBar => foo_bar)
 ;;   2. For Java -> string-inflection-java-style-cycle  (fooBar  => FOO_BAR => FooBar => fooBar)
-;;   3. Other    -> string-inflection-all-cycle         (foo_bar => FOO_BAR => FooBar => fooBar => foo-bar => foo_bar)
+;;   3. For All  -> string-inflection-all-cycle         (foo_bar => FOO_BAR => FooBar => fooBar => foo-bar => foo_bar)
 ;;
-;; Setting example
 ;;
-;;   (require 'string-inflection)
-;;   (global-unset-key (kbd "C-q"))
-;;   (global-set-key (kbd "C-q C-u") 'string-inflection-ruby-style-cycle) ; Vz Editor-like key binding
-;;
-;;   or
+;; Setting Example 1
 ;;
 ;;   (require 'string-inflection)
 ;;   (global-unset-key (kbd "C-q"))
+;;   ;; C-q C-u is the key bindings similar to Vz Editor.
 ;;   (global-set-key (kbd "C-q C-u") 'my-string-inflection-cycle-auto)
-;;
+;;   
 ;;   (defun my-string-inflection-cycle-auto ()
-;;     "switching in major-mode"
+;;     "switching by major-mode"
 ;;     (interactive)
 ;;     (cond
+;;      ;; for emacs-lisp-mode
 ;;      ((eq major-mode 'emacs-lisp-mode)
 ;;       (string-inflection-all-cycle))
+;;      ;; for java
 ;;      ((eq major-mode 'java-mode)
 ;;       (string-inflection-java-style-cycle))
 ;;      (t
+;;       ;; default
 ;;       (string-inflection-ruby-style-cycle))))
+;; 
+;;
+;; Setting Example 2
+;;
+;;   (require 'string-inflection)
+;;   
+;;   ;; default
+;;   (global-set-key (kbd "C-c C-u") 'string-inflection-all-cycle)
+;;   
+;;   ;; for ruby
+;;   (add-hook 'ruby-mode-hook
+;;             '(lambda ()
+;;                (local-set-key (kbd "C-c C-u") 'string-inflection-ruby-style-cycle)))
+;;   
+;;   ;; for java
+;;   (add-hook 'java-mode-hook
+;;             '(lambda ()
+;;                (local-set-key (kbd "C-c C-u") 'string-inflection-java-style-cycle)))
+;;
 
 ;;; Code:
 
